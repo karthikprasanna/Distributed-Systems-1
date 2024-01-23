bb9d9802a617efa4fb8b67f591d231e31e4752d4
Karthik Prasanna N <karthikprasanna@pop-os.localdomain>
2024-01-24 02:03:13 +0530

    Q3 is working. Hurray.

--- Q3.cpp
+++ Q3.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-bool is_alive(int **grid, int* top, int* bottom, int N, int M, int i, int j)
+bool is_alive(int **grid, int *top, int *bottom, int N, int M, int i, int j)
 {
     if (i >= 0 && i < N)
     {
@@ -11,7 +11,6 @@ bool is_alive(int **grid, int* top, int* bottom, int N, int M, int i, int j)
         {
             return grid[i][j];
         }
-        
     }
     else if (i == -1 && top)
     {
@@ -27,20 +26,16 @@ bool is_alive(int **grid, int* top, int* bottom, int N, int M, int i, int j)
             return bottom[j];
         }
     }
-    
+
     return 0;
 }
 
-int num_neighbours(int **grid, int* top, int* bottom, int N, int M, int i, int j)
+int num_neighbours(int **grid, int *top, int *bottom, int N, int M, int i, int j)
 {
-    return is_alive(grid, top, bottom, N, M, i-1, j-1)+is_alive(grid, top, bottom, N, M, i-1, j)+is_alive(grid, top, bottom, N, M, i-1, j+1)
-        +is_alive(grid, top, bottom, N, M, i+1, j-1)+is_alive(grid, top, bottom, N, M, i+1, j)+is_alive(grid, top, bottom, N, M, i+1, j+1)
-            +is_alive(grid, top, bottom, N, M, i, j-1)+is_alive(grid, top, bottom, N, M, i, j+1);
-    
+    return is_alive(grid, top, bottom, N, M, i - 1, j - 1) + is_alive(grid, top, bottom, N, M, i - 1, j) + is_alive(grid, top, bottom, N, M, i - 1, j + 1) + is_alive(grid, top, bottom, N, M, i + 1, j - 1) + is_alive(grid, top, bottom, N, M, i + 1, j) + is_alive(grid, top, bottom, N, M, i + 1, j + 1) + is_alive(grid, top, bottom, N, M, i, j - 1) + is_alive(grid, top, bottom, N, M, i, j + 1);
 }
 
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     MPI_Init(&argc, &argv);
 
@@ -52,200 +47,188 @@ int main(int argc, char* argv[])
 
     int recieve_configurations;
     int N, M, T;
-    int **grid, **gathered;
-   
-    int *flatten_send, *flatten_recieve, *buffer;
+    int **grid;
+
+    int *flatten_send, *buffer;
     int counts_send[size], displacements[size], count_recieve;
 
     if (my_rank == 0)
     {
-        cin>>N>>M>>T;
-        flatten_send = (int*)malloc(N * M * sizeof(int));
-        grid = (int**)malloc(N * sizeof(int*));
+        cin >> N >> M >> T;
+        flatten_send = (int *)malloc(N * M * sizeof(int));
+        grid = (int **)malloc(N * sizeof(int *));
         for (int i = 0; i < N; ++i)
         {
             grid[i] = &flatten_send[M * i];
             for (int j = 0; j < M; j++)
             {
-                cin>>grid[i][j];
+                cin >> grid[i][j];
             }
-            
         }
 
         int block_size = N / size;
-        
+        int rem = N % size;
 
-        if (block_size * size != N)
+        for (int i = 0; i < size; i++)
         {
-            block_size++;
-        }
-
-        for (int i = 0; i < size; i++) {
-            counts_send[i] = block_size * M;
-            displacements[i] = i * block_size * M;
-
-            if (i >= N)
+            if (rem)
             {
-                counts_send[i] = 0;
+                counts_send[i] = (block_size + 1) * M;
+                rem--;
+            }
+            else
+            {
+                if (i >= N)
+                {
+                    counts_send[i] = 0;
+                }
+                else
+                    counts_send[i] = block_size * M;
+            }
+            if (i == 0 || i >= N)
+            {
                 displacements[i] = 0;
             }
+            else
+                displacements[i] = displacements[i - 1] + counts_send[i - 1];
         }
-
-        if (N % size != 0)
-        {
-            counts_send[size - 1] = N * M - block_size * (size - 1) * M;
-        }
-        
-        flatten_recieve = (int*)malloc(N * M * sizeof(int));
-        gathered = (int**)malloc(N * sizeof(int*));
-        for (int i = 0; i < N; ++i)
-        {
-            gathered[i] = flatten_recieve + i * M;
-        }
-
     }
     MPI_Bcast(&counts_send, size, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&M, 1, MPI_INT, 0, MPI_COMM_WORLD);
- 
+    MPI_Bcast(&T, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
     count_recieve = counts_send[my_rank];
-    buffer = (int*)malloc(count_recieve * sizeof(int));
-    if (my_rank == 0)
-    MPI_Scatterv(grid[0], counts_send, displacements, MPI_INT, buffer, count_recieve, MPI_INT, 0, MPI_COMM_WORLD);
-    else
-    MPI_Scatterv(NULL, NULL, NULL, MPI_INT, buffer, count_recieve, MPI_INT, 0, MPI_COMM_WORLD);
-    
     int n = count_recieve / M;
-    
-    int** block = (int**)malloc(n * sizeof(int*));
-    int** simulated = (int**)malloc(n * sizeof(int*));
-    int *flatten_simulated = (int*)malloc(count_recieve * sizeof(int));
-    int *flatten_block = (int*)malloc(count_recieve * sizeof(int));
+
+    int **block = (int **)malloc(n * sizeof(int *));
+    int **simulated = (int **)malloc(n * sizeof(int *));
+    int *flatten_simulated = (int *)malloc(count_recieve * sizeof(int));
+    int *flatten_block = (int *)malloc(count_recieve * sizeof(int));
     int *bottom = NULL;
     int *top = NULL;
 
     if (my_rank > 0)
     {
-        top = (int*)malloc(M * sizeof(int));
+        top = (int *)malloc(M * sizeof(int));
     }
-    
+
     if (my_rank < size - 1)
     {
-        bottom = (int*)malloc(M * sizeof(int));
-    }
-    
-
-    for (int i = 0; i < n; i++)
-    {
-        block[i] = flatten_block + i * M;
-        simulated[i] = flatten_simulated + i * M;
-        for (int j = 0; j < M; j++)
-        {
-            block[i][j] = buffer[i * M + j];
-        }
-        
+        bottom = (int *)malloc(M * sizeof(int));
     }
 
-    if (my_rank % 2 == 0)
+    buffer = (int *)malloc(count_recieve * sizeof(int));
+    while (T--)
     {
-        if (my_rank > 0)
-        {
-            MPI_Send(block[0], M, MPI_INT, my_rank - 1, 0, MPI_COMM_WORLD);
-            MPI_Recv(top, M, MPI_INT, my_rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        }
 
-        if (my_rank < size - 1)
-        {
-            MPI_Send(block[n - 1], M, MPI_INT, my_rank + 1, 0, MPI_COMM_WORLD);
-            MPI_Recv(bottom, M, MPI_INT, my_rank + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        if (my_rank == 0)
+            MPI_Scatterv(grid[0], counts_send, displacements, MPI_INT, buffer, count_recieve, MPI_INT, 0, MPI_COMM_WORLD);
+        else
+            MPI_Scatterv(NULL, NULL, NULL, MPI_INT, buffer, count_recieve, MPI_INT, 0, MPI_COMM_WORLD);
 
-        }
-    }
-    else
-    {
-        if (my_rank > 0)
+        for (int i = 0; i < n; i++)
         {
-            MPI_Recv(top, M, MPI_INT, my_rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            MPI_Send(block[0], M, MPI_INT, my_rank - 1, 0, MPI_COMM_WORLD);
-        }
-
-        if (my_rank < size - 1)
-        {
-            MPI_Recv(bottom, M, MPI_INT, my_rank + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            MPI_Send(block[n - 1], M, MPI_INT, my_rank + 1, 0, MPI_COMM_WORLD);
-
-        }
-    }
-
-    // cout<<endl;
-    // cout<<top<<bottom<<endl;
-    int count;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < M; j++)
-        {
-            count = num_neighbours(block, top, bottom, n, M, i, j);
-            // cout<<count<<' ';
-            if (is_alive(block, top, bottom, n, M, i, j))
+            block[i] = flatten_block + i * M;
+            simulated[i] = flatten_simulated + i * M;
+            for (int j = 0; j < M; j++)
             {
-                if (count < 2 || count > 3)
+                block[i][j] = buffer[i * M + j];
+            }
+        }
+
+        if (my_rank % 2 == 0)
+        {
+            if (my_rank > 0)
+            {
+                MPI_Send(block[0], M, MPI_INT, my_rank - 1, 0, MPI_COMM_WORLD);
+                MPI_Recv(top, M, MPI_INT, my_rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            }
+
+            if (my_rank < size - 1)
+            {
+                MPI_Send(block[n - 1], M, MPI_INT, my_rank + 1, 0, MPI_COMM_WORLD);
+                MPI_Recv(bottom, M, MPI_INT, my_rank + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            }
+        }
+        else
+        {
+            if (my_rank > 0)
+            {
+                MPI_Recv(top, M, MPI_INT, my_rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                MPI_Send(block[0], M, MPI_INT, my_rank - 1, 0, MPI_COMM_WORLD);
+            }
+
+            if (my_rank < size - 1)
+            {
+                MPI_Recv(bottom, M, MPI_INT, my_rank + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                MPI_Send(block[n - 1], M, MPI_INT, my_rank + 1, 0, MPI_COMM_WORLD);
+            }
+        }
+
+        int count;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < M; j++)
+            {
+                count = num_neighbours(block, top, bottom, n, M, i, j);
+                if (is_alive(block, top, bottom, n, M, i, j))
                 {
-                    simulated[i][j] = 0;
+                    if (count < 2 || count > 3)
+                    {
+                        simulated[i][j] = 0;
+                    }
+                    else
+                    {
+                        simulated[i][j] = 1;
+                    }
                 }
                 else
                 {
-                    simulated[i][j] = 1;
+                    if (count == 3)
+                    {
+                        simulated[i][j] = 1;
+                    }
+                    else
+                    {
+                        simulated[i][j] = 0;
+                    }
                 }
-            }
-            else
-            {
-                if (count == 3)
-                {
-                    simulated[i][j] = 1;
-                }
-                else
-                {
-                    simulated[i][j] = 0;
-                }
-                
             }
         }
-    // cout<<endl;    
-    }
+        if (my_rank == 0)
+        {
 
+            MPI_Gatherv(simulated[0], count_recieve, MPI_INT, grid[0], counts_send, displacements, MPI_INT, 0, MPI_COMM_WORLD);
+        }
+        else
+        {
+            MPI_Gatherv(simulated[0], count_recieve, MPI_INT, NULL, NULL, NULL, MPI_INT, 0, MPI_COMM_WORLD);
+        }
+    }
     if (my_rank == 0)
     {
-        MPI_Gatherv(simulated[0], count_recieve, MPI_INT, gathered[0], counts_send, displacements, MPI_INT, 0, MPI_COMM_WORLD);
-        cout<<endl;
+        cout << endl;
         for (int i = 0; i < N; i++)
         {
             for (int j = 0; j < M; j++)
             {
-                cout<<gathered[i][j]<<' ';
+                cout << grid[i][j] << ' ';
             }
-            cout<<endl;
-            
+            cout << endl;
         }
         free(flatten_send);
-        free(flatten_recieve);
-        free(flatten_block);
-        free(flatten_simulated);
-        free(top);
-        free(bottom);
-        free(block);
-        free(simulated);
-        free(gathered);
         free(grid);
     }
-    else
-    {
-        MPI_Gatherv(simulated[0], count_recieve, MPI_INT,NULL, NULL, NULL, MPI_INT, 0, MPI_COMM_WORLD);
-    }
-    
+    free(flatten_block);
+    free(flatten_simulated);
+    free(top);
+    free(bottom);
+    free(block);
+    free(simulated);
     free(buffer);
-
     MPI_Finalize();
-    return EXIT_SUCCESS;    
+    return EXIT_SUCCESS;
 }
 
 /*
@@ -269,4 +252,3 @@ int main(int argc, char* argv[])
 
 
 */
-
