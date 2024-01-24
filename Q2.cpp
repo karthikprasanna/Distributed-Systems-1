1f1824b6ef05f6656f80d8b1532355036c3b500d
Karthik Prasanna N <karthikprasanna@pop-os.localdomain>
2024-01-24 11:18:09 +0530

    q2 is working to some extent

--- Q2.cpp
+++ Q2.cpp
@@ -3,23 +3,7 @@
 
 using namespace std;
 
-void calculateShortestDistance(vector<vector<int>> A, vector<vector<int>> A_Final, int k)
-{
-    int N = A.size();
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            if (A_Final[i][k] != INT_MAX && A_Final[k][j] != INT_MAX)
-                A_Final[i][j] = min(A_Final[i][j], A_Final[i][k] + A_Final[k][j]);
-            
-        }
-        
-    }
-    
-}
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     MPI_Init(&argc, &argv);
 
@@ -29,54 +13,159 @@ int main(int argc, char* argv[])
     int my_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-    int P = size;
     int N;
-    cin>>N;
+    int **grid;
 
-    vector<vector<int>> A(N, vector<int>(N));
-    vector<vector<int>> A_Final(N, vector<int>(N));
+    int *flatten_send, *buffer;
+    int counts_send[size], displacements[size], count_recieve;
 
-    for (int i = 0; i < N; ++i)
+    if (my_rank == 0)
     {
-        for (int j = 0; j < N; ++j)
+        cin >> N;
+        flatten_send = (int *)malloc(N * N * sizeof(int));
+        grid = (int **)malloc(N * sizeof(int *));
+
+        for (int i = 0; i < N; ++i)
         {
-            cin>>A[i][j];
-            if (A[i][j] == -1)
+            grid[i] = &flatten_send[N * i];
+            for (int j = 0; j < N; j++)
             {
-                A_Final[i][j] = INT_MAX;
+                cin >> grid[i][j];
+            }
+        }
+
+        int block_size = N / size;
+        int rem = N % size;
+
+        for (int i = 0; i < size; i++)
+        {
+            if (rem)
+            {
+                counts_send[i] = (block_size + 1) * N;
+                rem--;
             }
             else
             {
-                A_Final[i][j] = A[i][j];
+                if (i >= N)
+                {
+                    counts_send[i] = 0;
+                }
+                else
+                    counts_send[i] = block_size * N;
             }
+            if (i == 0 || i >= N)
+            {
+                displacements[i] = 0;
+            }
+            else
+                displacements[i] = displacements[i - 1] + counts_send[i - 1];
         }
     }
+    MPI_Bcast(&counts_send, size, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    count_recieve = counts_send[my_rank];
+    int n = count_recieve / N;
+
+    int **block = (int **)malloc(n * sizeof(int *));
+    int *flatten_block = (int *)malloc(count_recieve * sizeof(int));
+
+    int *row = (int *)malloc(N * sizeof(int));
+
+    int *column = (int *)malloc(N * sizeof(int));
+
+    buffer = (int *)malloc(count_recieve * sizeof(int));
+
     for (int k = 0; k < N; k++)
     {
-        calculateShortestDistance(A, A_Final, k);
+
+        if (my_rank == 0)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                row[i] = grid[k][i];
+                column[i] = grid[i][k];
+            }
+
+            MPI_Scatterv(grid[0], counts_send, displacements, MPI_INT, buffer, count_recieve, MPI_INT, 0, MPI_COMM_WORLD);
+        }
+        else
+            MPI_Scatterv(NULL, NULL, NULL, MPI_INT, buffer, count_recieve, MPI_INT, 0, MPI_COMM_WORLD);
+
+        MPI_Bcast(row, N, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(column, N, MPI_INT, 0, MPI_COMM_WORLD);
+        for (int i = 0; i < n; i++)
+        {
+            block[i] = flatten_block + i * N;
+            for (int j = 0; j < N; j++)
+            {
+                block[i][j] = buffer[i * N + j];
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                if (column[i] != -1 && row[j] != -1)
+                {
+                    if (block[i][j] == -1)
+                    {
+                        block[i][j] = column[i] + row[j];
+                    }
+                    else
+                        block[i][j] = min(block[i][j], column[i] + row[j]);
+                }
+            }
+        }
+
+        if (my_rank == 0)
+        {
+
+            MPI_Gatherv(block[0], count_recieve, MPI_INT, grid[0], counts_send, displacements, MPI_INT, 0, MPI_COMM_WORLD);
+        }
+        else
+        {
+            MPI_Gatherv(block[0], count_recieve, MPI_INT, NULL, NULL, NULL, MPI_INT, 0, MPI_COMM_WORLD);
+        }
     }
 
     if (my_rank == 0)
     {
-        for (int i = 0; i < N; ++i)
+        cout << endl;
+        for (int i = 0; i < N; i++)
         {
-            for (int j = 0; j < N; ++j)
+            for (int j = 0; j < N; j++)
             {
-                if (A_Final[i][j] != INT_MAX)
-                {
-                    cout<<A_Final[i][j]<<" ";
-                }
-                else
-                {
-                    cout<<"-1 ";
-                }                
-
+                cout << grid[i][j] << ' ';
             }
-            cout<<endl;
-        }   
+            cout << endl;
+        }
+        free(flatten_send);
+        free(grid);
     }
-    
+
+    free(flatten_block);
+    free(row);
+    free(column);
+    free(block);
+    free(buffer);
     MPI_Finalize();
-    return 0;    
+    return EXIT_SUCCESS;
 }
 
+/*
+2
+0 25
+-1 0
+
+3
+0 1 43
+1 0 6
+-1 -1 0
+
+4
+0 5 -1 10
+-1 0 3 -1
+-1 -1 0 1
+-1 -1 -1 0
+*/
